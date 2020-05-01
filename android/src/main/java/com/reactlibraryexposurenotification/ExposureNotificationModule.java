afaa2a632cb1f4a93742b7f18342aaf99658917f
Jam Risser <jam@codejam.ninja>
2020-05-01 14:27:09 +0000

    Setup gradle

--- android/src/main/java/com/reactlibraryexposurenotification/ExposureNotificationModule.java
+++ android/src/main/java/com/reactlibraryexposurenotification/ExposureNotificationModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.reactlibraryexposurenotification;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
